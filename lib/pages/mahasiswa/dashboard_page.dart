a6b3517034055a66b884ca7b92457650b86e41d0
malfaza <alfmalvza@gmail.com>
2024-02-07 06:13:57 +0700

    tambah tombol logout

--- lib/pages/mahasiswa/dashboard_page.dart
+++ lib/pages/mahasiswa/dashboard_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../bloc/logout/logout_bloc.dart';
+import '../../common/constants/colors.dart';
+import '../../data/datasources/auth_local_datasource.dart';
+import '../auth/auth_page.dart';
 import '../../common/components/menu_card.dart';
 import '../../common/components/search_input.dart';
-import '../../common/constants/colors.dart';
 import '../../common/constants/images.dart';
 import 'absensi_page.dart';
 import 'jadwal_matkul_page.dart';
@@ -33,6 +37,51 @@ class _DashboardPageState extends State<DashboardPage> {
                 fontSize: 24,
                 fontWeight: FontWeight.w700,
                 color: ColorName.primary,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            Center(
+              child: BlocProvider(
+                create: (context) => LogoutBloc(),
+                child: BlocConsumer<LogoutBloc, LogoutState>(
+                  listener: (context, state) {
+                    state.maybeWhen(
+                        orElse: () {},
+                        loaded: () {
+                          AuthLocalDatasource().removeAuthData();
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const AuthPage();
+                          }));
+                        },
+                        error: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Logout Error')));
+                        });
+                  },
+                  builder: (context, state) {
+                    return state.maybeWhen(orElse: () {
+                      return ElevatedButton(
+                        onPressed: () {
+                          context
+                              .read<LogoutBloc>()
+                              .add(const LogoutEvent.logout());
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: ColorName.white,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text('Logout'),
+                        ),
+                      );
+                    }, loaded: () {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    });
+                  },
+                ),
               ),
             ),
             Row(
